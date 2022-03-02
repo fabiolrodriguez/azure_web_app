b0b3c9f2f2503b64c90684dd5ae2c9a82c169802
fabiolrodriguez <fabio.rodriguez@gmail.com>
2022-03-02 19:12:42 -0300

    add output to publish profile

--- terraform/sql.tf
+++ terraform/sql.tf
@@ -16,8 +16,6 @@ resource "azurerm_mssql_database" "base" {
   name           = var.database_name
   server_id      = azurerm_mssql_server.sqlserver.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-#   max_size_gb    = 4
   read_scale     = false
   sku_name       = "S0"
   zone_redundant = false
